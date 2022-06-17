3e05810d58d1de5248d322ded9e79576a8d6df7c
wusg <wusg@go-goal.com>
2022-06-17 15:37:49 +0800

    6-17

--- src/pojo/Block.ts
+++ src/pojo/Block.ts
@@ -1,6 +1,6 @@
-import type { VueConstructor, VNode } from 'vue'
+import type { VueConstructor, VNode, Component, AsyncComponent } from 'vue'
+import Utils from './Utils'
 import Vue from 'vue'
-import { Page } from './Page'
 
 export enum BlockType {
     Extru, // 挤压
@@ -8,100 +8,125 @@ export enum BlockType {
 }
 
 export interface Entity extends Vue {
-    show: boolean
+    isSelected: boolean
 }
-
+/**
+ * Block 组件与可操作壳的整体
+ */
 export class Block {
-    private entity: Entity
+    // 当前block的vue实例
+    private blockEntity: Entity
 
-    // 寄宿的page实例
-    private page: Page
+    // 布局信息
+    private layout: [number, number, number, number] = [0, 0, 0, 0]
 
-    constructor(id: string, type: BlockType, vue: Vue, page: Page) {
-        this.page = page
-        this.createBlock(id, type, vue)
-    }
-
-    private createBlock(id: string, type: BlockType, vue: Vue) {
-        console.log(type)
-        const _this = this
+    /**
+     * 根据组件生成相应的block，返回dom节点
+     * @param component 即将渲染的组件
+     * @param clickEvent block点击事件
+     * @param dropEvent block着陆事件
+     * @returns 真实dom节点
+     */
+    public createElement(
+        component: Component<any, any, any, any> | AsyncComponent<any, any, any, any>, 
+        clickEvent: (e: Event) => void, 
+        dropEvent: (e: DragEvent) => void
+    ): Element {
         const ComponentCtr: VueConstructor = Vue.extend({
             data() {
                 return {
-                    show: false
+                    isSelected: false, // 是否选中
+                    tipAreaIndex: -1 // 当前展示提示的阴影区域 0上 1右 2下 3左
                 }
             },
             render(h): VNode {
-                const maskStyle = {
-                    position: 'absolute',
-                    background: '#000',
-                    opacity: 0.3
-                }
+                const AREA_CONFIG: [string, string][] = [
+                    ['0 0 0 50%', 'polygon(100% 0, 50% 50%, 100% 100%)', ],
+                    ['50% 0 0 0', 'polygon(0 100%, 50% 50%, 100% 100%)'],
+                    ['0 50% 0 0', 'polygon(0 0, 50% 50%, 0 100%)'],
+                    ['0 0 50% 0', 'polygon(0 0, 50% 50%, 100% 0)']
+                ]
+                const tipArea: VNode[] = []
+                const determineArea: VNode[] = []
+                const lineArea: VNode[] = []
+                AREA_CONFIG.forEach((item: [string, string], index: number) => {
+                    tipArea.push(h('div', {
+                        style: {
+                            position: 'absolute',
+                            background: '#000',
+                            inset: item[0],
+                            transition: 'opacity .3s',
+                            opacity: this.tipAreaIndex === index ? '0.3' : '0'
+                        }
+                    }))
+                    determineArea.push(h('div', {
+                        style: {
+                            position: 'absolute',
+                            inset: 0,
+                            opacity: 0,
+                            clipPath: item[1]
+                        },
+                        on: {
+                            dragover: () => this.tipAreaIndex = index,
+                            dragleave: () => this.tipAreaIndex = -1,
+                            drop: (e: DragEvent) => {
+                                this.tipAreaIndex = -1
+                                dropEvent(e)
+                            }
+                        }
+                    }))
+                    // lineArea.push(h('div', {
+                    //     style: {
+                    //         position: 'absolute',
+                    //         height: '2px',
+                    //         top: 0,
+                    //         width: 100%
+                    //     }
+                    // }))
+                })
                 return h('div', {
                     attrs: {
-                        draggable: this.show
+                        draggable: this.isSelected
                     },
                     style: {
                         position: 'absolute',
                     }
                 }, [
-                    h(vue.$options.components[id]),
+                    h(component),
                     h('div', {
                         style: {
                             position: 'absolute',
                             inset: 0,
-                            boxSizing: 'border-box',
-                            border: '2px solid red',
-                            opacity: this.show ? 1 : 0
+                            boxSizing: 'border-box'
                         },
                         on: {
                             click: (e: Event) => {
-                                if (window.event) { 
-                                    // ie 和 谷歌支持阻止冒泡
-                                    window.event.cancelBubble = true; 
-                                } else { 
-                                    // 火狐和谷歌支持的阻止冒泡
-                                    e.preventDefault(); 
-                                } 
-                                _this.page.selectBlock(_this)
+                                Utils.stopBubble(e)
+                                clickEvent(e)
                             }
                         }
                     }, [
                         h('div', {
                             style: {
-                                ...maskStyle,
-                                inset: '0 50% 0 0'
-                            },
-                        }),
-                        h('div', {
-                            style: {
-                                ...maskStyle,
-                                inset: '0 0 0 50%'
+                                position: 'absolute',
+                                inset: 0,
+                                border: '2px solid red',
+                                opacity: this.isSelected ? 1 : 0
                             }
                         }),
-                        h('div', {
-                            style: {
-                                ...maskStyle,
-                                inset: '0 0 50% 0'
-                            }
-                        }),
-                        h('div', {
-                            style: {
-                                ...maskStyle,
-                                inset: '50% 0 0 0'
-                            }
-                        })
+                        ...tipArea,
+                        ...determineArea
+                        
                     ])
                 ])
             }
         })
-        this.entity = new ComponentCtr()
-        this.page.insertDomTree(this.entity.$mount().$el)
-        this.page.selectBlock(this)
+        this.blockEntity = new ComponentCtr()
+        return this.blockEntity.$mount().$el
     }
 
-    public setShow(show: boolean) {
-        console.log('setShow', show)
-        this.entity.show = show
+    // 选中该block
+    public setSelected(isSelected: boolean) {
+        this.blockEntity.isSelected = isSelected
     }
 }
--- src/pojo/Page.ts
+++ src/pojo/Page.ts
@@ -1,59 +1,73 @@
 import { Block, BlockType } from './Block'
+import Utils from './Utils'
 import Vue from 'vue'
 
 export class Page  {
-    // 页面寄宿的元素dom
+    // 宿主dom节点
     private $el: HTMLElement
 
-    // 当前处于的vue实例
-    private entity: Vue
+    // 宿主节点处于的vue实例，需要获取该实例中已注册的组件
+    private vueEntity: Vue
 
-    // 存储当前页面上的所有block
-    private blocks: Map<string, Block> = new Map()
+    // block存储仓库
+    private blocksDepo: Map<string, Block> = new Map()
 
     // 当前选中的block
     private currentBlock: Block | null = null
     
-    constructor(el: HTMLElement, entity: Vue) {
+    constructor(el: HTMLElement, vueEntity: Vue) {
         this.$el = el
         this.$el.style.position = 'relative' // 保证是相对定位
-        this.entity = entity
+        this.vueEntity = vueEntity
         this.setTargetListener()
     }
 
     // 设置寄宿的元素的监听事件
     private setTargetListener() {
-        this.$el.addEventListener('dragover', e => e.preventDefault())
-        this.$el.addEventListener('drop', e => this.insertBlock(e))
+        this.$el.addEventListener('dragover', (e: DragEvent) => e.preventDefault())
+        this.$el.addEventListener('drop', (e: DragEvent) => {
+            console.log('page drop')
+            this.drop(e)
+        })
         this.$el.addEventListener('click', () => this.blankClick())
     }
 
-    // 插入block
-    private insertBlock(e: DragEvent) {
-        const {id, type}: {id: string, type: BlockType} = JSON.parse(e.dataTransfer.getData('config'))
-        if (this.blocks.has(id)) return // 重复组件不再新建
-        this.currentBlock = new Block(id, type, this.entity, this) // 创建block实例
-        this.blocks.set(id, this.currentBlock) // 存入仓库
-    }
-
-    // 插入dom树
-    public insertDomTree(el: Element) {
-        this.$el.append(el)
+    // 组件drop事件
+    private drop(e: DragEvent) {
+        const config: string = e.dataTransfer.getData('config')
+        if (config !== '') {
+            const {id, type}: {id: string, type: BlockType} = JSON.parse(config)
+            if (this.blocksDepo.has(id)) return // 重复组件不再新建
+            // 创建block实例
+            const block = new Block()
+            this.$el.append(block.createElement(
+                this.vueEntity.$options.components[id], 
+                (e) => this.selectBlock(block), 
+                (e: DragEvent) => {
+                    Utils.stopBubble(e)
+                    console.log('block', id, 'drop')
+                    this.drop(e)
+                }
+            ))
+            this.selectBlock(block) // 选中新创建的block
+            this.blocksDepo.set(id, block) // 存入仓库
+            
+        }
     }
 
     // page空白部分点击事件
     private blankClick() {
         if (this.currentBlock !== null) {
-            this.currentBlock.setShow(false)
+            this.currentBlock.setSelected(false)
         }
         this.currentBlock = null
     }
 
-    // 选中page上的block
-    public selectBlock(block: Block) {
-        console.log('setBlock')
+    // 选中block
+    private selectBlock(block: Block) {
+        block.setSelected(true) // 聚焦选中的block
         this.currentBlock = block
-        this.currentBlock.setShow(true)
+        
     }
 
     public remove() {
